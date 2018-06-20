79c4a372ef01c874ee3f4256b03c99a7dadb28cf
Taspyn <taspyn.akirachix@gmail.com>
2018-06-20 15:45:17 +0300

    my working to do app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container">
     <h3>{{ title }}</h3>
+    <form>
+    	<textarea placeholder="add new task" v-model="newTask"></textarea>
+    	<p>{{newTask}}</p>
+    	<button type="button" v-on:click="addTask">Add Task{{tasks.length}}</button>
+    </form>
+    <ul> <li v-for="task in tasks ">{{task}}</li>
+
+    </ul>
+
   </div>
 </template>
 
@@ -8,8 +17,23 @@
 export default {
   data: function() {
     return {
-      title: 'My App'
+      title: 'My Awesome App',
+      newTask: 'This is a Sample',
+     
+      tasks:[
+
+      ]
     }
+  },
+  methods: {
+  	addTask(){
+  	
+  		this.tasks.unshift(this.newTask);
+  		this.newTask = '';
+  		this.tasks.length;
+
+  	}
   }
+
 }
 </script>
